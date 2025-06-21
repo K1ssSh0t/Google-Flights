9ea00c44195c56a593d46e7987a578249d31064a
K1ssSh0t <angelomarmatias@gmail.com>
2025-06-21 22:21:03 +0000

    feat: add airport search interfaces for Daum functionality

--- src/types.ts
+++ src/types.ts
@@ -213,3 +213,39 @@ export interface FlightSearchParams {
   market: "en-US";
   countryCode: "US";
 }
+
+// Airport search
+
+export interface Daum {
+  skyId: string;
+  entityId: string;
+  presentation: Presentation;
+  navigation: Navigation;
+}
+
+export interface Presentation {
+  title: string;
+  suggestionTitle: string;
+  subtitle: string;
+}
+
+export interface Navigation {
+  entityId: string;
+  entityType: string;
+  localizedName: string;
+  relevantFlightParams: RelevantFlightParams;
+  relevantHotelParams: RelevantHotelParams;
+}
+
+export interface RelevantFlightParams {
+  skyId: string;
+  entityId: string;
+  flightPlaceType: string;
+  localizedName: string;
+}
+
+export interface RelevantHotelParams {
+  entityId: string;
+  entityType: string;
+  localizedName: string;
+}
